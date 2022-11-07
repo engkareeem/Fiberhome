c7583ab757dd7c8506b51c8b6904d132547f48a1
nasrallah <naser2010net@gmail.com>
2022-11-07 15:09:57 +0200

    Testing new Animation 2

--- src/main/java/paltel/fiber/fiberhome/testing/testingController.java
+++ src/main/java/paltel/fiber/fiberhome/testing/testingController.java
@@ -1,14 +1,14 @@
 package paltel.fiber.fiberhome.testing;
 
-import animatefx.animation.Bounce;
-import animatefx.animation.BounceIn;
-import animatefx.animation.ZoomIn;
-import animatefx.animation.ZoomOut;
+import animatefx.animation.*;
 import javafx.animation.Animation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
@@ -27,6 +27,9 @@ public class testingController implements Initializable {
     @FXML
     AnchorPane ap;
     double xOffset,yOffset;
+
+    @FXML
+    Button loginButton;
     @FXML
     Pane titleBar;
 
@@ -34,11 +37,46 @@ public class testingController implements Initializable {
     Pane loginPopup;
     @FXML
     ImageView imageView;
+
+    @FXML
+    Label loginTitle;
+    @FXML
+    Label welcomebacklable;
+
+    @FXML
+    TextField emailInput;
+
+    @FXML
+    Label passwordLabel;
+    @FXML
+    TextField passwordInput;
+
+    @FXML
+    Label forgetPasswordLabel;
+    @FXML
+    Label emailUserNameLabel;
+
+    @FXML
+    Label notRegisterLabel;
+
+    @FXML
+    Label registerLabel;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ZoomIn stageZoom = new ZoomIn(ap);
         titleBar.setOpacity(0);
-        stageZoom.setOnFinished((actionEvent) -> new ZoomIn(titleBar).play());
+        stageZoom.setOnFinished((actionEvent) -> {
+                new ZoomIn(titleBar).play();
+            new FadeInDown(welcomebacklable).play();
+            new FadeInDown(loginTitle).play();
+            Animator.chainAnimator(new FadeInDown(emailUserNameLabel), new FadeInDown(emailInput));
+            Animator.chainAnimator( new FadeInDown(passwordLabel) ,new FadeInDown(passwordInput) );
+            new FadeInDown(forgetPasswordLabel).play();
+            new FadeInDown(loginButton).play();
+            Animator.chainAnimator(new FadeInDown(notRegisterLabel), new FadeInDown(registerLabel));
+        });
+
+
         stageZoom.play();
         Rectangle clip = new Rectangle(
                 imageView.getFitWidth(), imageView.getFitHeight()
@@ -76,5 +114,19 @@ public class testingController implements Initializable {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
